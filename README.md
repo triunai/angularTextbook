ba6aa92724357d786b1f2140af83381cf0b6e3b4
Khumeren R <khucode@gmail.com>
2023-09-22 16:38:21 +0800

    Navigation and routing, add guards next maybe

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularTextbook
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pm51qf-4ad1zs)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,32 @@
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+  
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+
+    if(!this.product) {
+      console.error("Product not found for productId: ",productIdFromRoute);
+      this.router.navigate(['/products']);
+  }
+
+}
+}
